0939ae3421fcaf42d5965b5f51f4c3b9e0ce629c
veerannadeshanur11 <shreyasdeshanur34@gmail.com>
2025-10-07 23:36:07 +0530

    added eligibility check feature

--- attendence.py
+++ attendence.py
@@ -8,3 +8,8 @@ attendance = (classes_attended / classes_held) * 100
 print("\nClasses Held:", classes_held)
 print("Classes Attended:", classes_attended)
 print("Attendance: {:.2f}%".format(attendance))
+
+if attendance >= 75:
+    print("Status: Eligible for exams")
+else:
+    print("Status: Not eligible for exams")
